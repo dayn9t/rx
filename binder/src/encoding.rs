c396072a14bad3f346d5c5a8bd3b2e7320f60b5a
dayn9t <dayn9t@gmail.com>
2020-03-06 15:03:26 +0800

    20-03-06

--- binder/src/encoding.rs
+++ binder/src/encoding.rs
@@ -1,17 +1,10 @@
 use encoding_rs::*;
 
 /// GBK 转 UTF8
-pub fn to_utf8(text: &[u8]) -> Option<String> {
-    if let Ok(s) = std::str::from_utf8(text) {
-        println!("encoding: UTF-8");
-        return Some(s.to_string());
-    }
-
+pub fn gbk_to_utf8(text: &[u8]) -> Option<Vec<u8>> {
     let (cow, _, had_err) = GBK.decode(text);
     if had_err {
-        println!("Had err!");
+//        println!("Had err!");
     }
-    //None
-
-    Some(cow.to_string())
+    Some(cow.as_bytes().to_vec())
 }
--- binder/src/main.rs
+++ binder/src/main.rs
@@ -1,6 +1,9 @@
 mod encoding;
 
+use encoding::gbk_to_utf8;
+
 use http_req::request;
+
 
 #[macro_use]
 extern crate html5ever;
@@ -8,6 +11,8 @@ extern crate html5ever;
 use html5ever::parse_document;
 use html5ever::rcdom::{Handle, NodeData, RcDom};
 use html5ever::tendril::TendrilSink;
+use html5ever::LocalName;
+
 
 // This is not proper HTML serialization, of course.
 
@@ -25,7 +30,7 @@ fn walk(indent: usize, handle: &Handle) {
         } => println!("<!DOCTYPE {} \"{}\" \"{}\">", name, public_id, system_id),
 
         NodeData::Text { ref contents } => {
-            println!("#text: {}", escape_default(&contents.borrow()))
+            println!("#text: -{}-", escape_default(&contents.borrow()))
         }
 
         NodeData::Comment { ref contents } => println!("<!-- {} -->", escape_default(contents)),
@@ -36,7 +41,7 @@ fn walk(indent: usize, handle: &Handle) {
             ..
         } => {
             assert!(name.ns == ns!(html));
-            print!("<{}", name.local);
+            print!("##<{}", name.local);
             for attr in attrs.borrow().iter() {
                 assert!(attr.name.ns == ns!());
                 print!(" {}=\"{}\"", attr.name.local, attr.value);
@@ -56,26 +61,86 @@ pub fn escape_default(s: &str) -> String {
     s.chars().flat_map(|c| c.escape_default()).collect()
 }
 
+fn find_attr(
+    handle: &Handle,
+    elem_name: &LocalName,
+    attr_name: &LocalName,
+) -> Option<String> {
+    let node = handle;
+
+    match node.data {
+        NodeData::Element {
+            ref name,
+            ref attrs,
+            ..
+        } => {
+            println!("name.local: {}", name.local);
+            if name.local == *elem_name {
+                for attr in attrs.borrow().iter() {
+                    if attr.name.local == *attr_name {
+                        return Some(attr.value.to_string());
+                    }
+                }
+            }
+        }
+        _ => {}
+    }
+
+
+    for child in node.children.borrow().iter() {
+        let value = find_attr(child, elem_name, attr_name);
+        if value.is_some() {
+            return value;
+        }
+    }
+
+    None
+}
+
+
+fn find_charset(handle: &Handle) -> Option<String> {
+    //let node = handle;
+    let content = find_attr(handle, &LocalName::from("meta"), &LocalName::from("content"))?;
+
+    let re = regex::Regex::new(r"charset=(\w+)").unwrap();
+    let cap = re.captures(&content)?;
+    Some(cap.get(1).unwrap().as_str().to_uppercase())
+}
+
+
+pub fn parse(data: &mut &[u8]) -> std::io::Result<RcDom> {
+    let dom = parse_document(RcDom::default(), Default::default())
+        .from_utf8()
+        .read_from(data)?;
+
+    if let Some(charset) = find_charset(&dom.document) {
+        println!("charset: {}", charset);
+        if charset == "GBK" {
+            let data = gbk_to_utf8(data).unwrap();
+            let dom = parse_document(RcDom::default(), Default::default())
+                .from_utf8()
+                .read_from(&mut &data[..])?;
+            return Ok(dom);
+        }
+    }
+    Ok(dom)
+}
+
+
 fn main() {
     //let url = "http://116.228.67.70:30080/";
     let url = "https://www.i7wx.com/book/54/54350/";
-    let mut writer = Vec::new(); //container for body of a response
-    let res = request::get(url, &mut writer).unwrap();
+    let mut data = Vec::new(); //container for body of a response
+    let res = request::get(url, &mut data).unwrap();
 
     println!("res: {}", res.headers());
 
     println!("Status: {} {}", res.status_code(), res.reason());
-    println!("Len: {}", writer.len());
+    println!("Len: {}", data.len());
 
-    if let Some(s) = encoding::to_utf8(&writer) {
-        println!("result: ##{}##", s);
-    }
 
-    let stdin = io::stdin();
-    let dom = parse_document(RcDom::default(), Default::default())
-        .from_utf8()
-        .read_from(&mut stdin.lock())
-        .unwrap();
+    let dom = parse(&mut &data[..]).unwrap();
+
     walk(0, &dom.document);
 
     if !dom.errors.is_empty() {
