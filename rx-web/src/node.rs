60d5121f3a937dcad5cd3f91a9e4c7b60de72200
dayn9t <dayn9t@gmail.com>
2020-06-23 23:00:28 +0800

    20-06-23

--- rx-web/src/node.rs
+++ rx-web/src/node.rs
@@ -33,9 +33,10 @@ impl Node {
     /// 拉取网页创建节点树
     pub fn pull(url: &str) -> Option<Node> {
         let mut data = Vec::new();
-        let resp = request::get(url, &mut data).ok()?;
-        println!("res: {}", resp.headers());
-        println!("Status: {} {}", resp.status_code(), resp.reason());
+        //let resp =
+        request::get(url, &mut data).ok()?;
+        //println!("res: {}", resp.headers());
+        //println!("Status: {} {}", resp.status_code(), resp.reason());
 
         let doc = html::Document::parse(&data[..]).ok()?;
         let root = build_from(&doc.dom.document);
