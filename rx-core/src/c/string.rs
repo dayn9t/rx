ec5f9a8cb195ce860e189e3c341d9fa464971bd9
dayn9t <dayn9t@gmail.com>
2024-08-03 00:27:03 +0800

    24-08-03

--- rx-core/src/c/string.rs
+++ rx-core/src/c/string.rs
@@ -1,6 +1,8 @@
 use deref_derive::{Deref, DerefMut};
 use std::ffi::{c_char, CString};
 use std::path::Path;
+use std::{io, ptr};
+use std::io::ErrorKind;
 
 /// CString 增强版本
 #[derive(Debug, Default, Deref, DerefMut)]
@@ -29,6 +31,31 @@ impl CStringX {
 pub fn cstr(s: impl Into<Vec<u8>>) -> CStringX {
     CStringX::new(s)
 }
+
+
+/// 字接数组 复制到 C字符数组
+pub fn bytes_fill_chars<const N: usize>(src: &[u8], dst: &mut [c_char; N]) -> io::Result<()> {
+    let len = src.len().min(N - 1);
+
+    if src.len() > N - 1 {
+        return Err(io::Error::new(ErrorKind::InvalidInput, "buffer length is insufficient"));
+    }
+
+    unsafe {
+        ptr::copy_nonoverlapping(src.as_ptr(), dst.as_mut_ptr() as *mut u8, len);
+    }
+    dst[len] = 0;
+    Ok(())
+}
+
+
+/// 字符串 转换为 C字符数组
+pub fn str_to_chars<const N: usize>(s: &str) -> io::Result<[c_char; N]> {
+    let mut c_array: [c_char; N] = [0; N];
+    bytes_fill_chars(s.as_bytes(), &mut c_array)?;
+    Ok(c_array)
+}
+
 
 /*
 impl Deref for StringAdapter {
@@ -75,5 +102,24 @@ mod tests {
         let wrapper = CStringX::new(s);
         let c_str = unsafe { CStr::from_ptr(wrapper.ptr()) };
         assert_eq!(c_str.to_str().unwrap(), s);
+    }
+
+
+    #[test]
+    fn test_bytes_fill_c_chars_success() {
+        let src = b"hello";
+        let mut dst = [0 as c_char; 6]; // 5 characters + null terminator
+        bytes_fill_chars(src, &mut dst).expect("Failed to fill c_chars");
+        let c_str = unsafe { CStr::from_ptr(dst.as_ptr()) };
+        assert_eq!(c_str.to_str().unwrap(), "hello");
+    }
+
+    #[test]
+    fn test_bytes_fill_c_chars_insufficient_buffer() {
+        let src = b"hello";
+        let mut dst = [0 as c_char; 5]; // Insufficient buffer
+        let result = bytes_fill_chars(src, &mut dst);
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err().kind(), io::ErrorKind::InvalidInput);
     }
 }
